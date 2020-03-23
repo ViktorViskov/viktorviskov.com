eb07b2997227034d217401b82c401d4dde0a8a81
Viktor_Viskov <carrergt@gmail.com>
2020-03-23 16:56:29 +0100

    fix small bug in main forlder

--- src/main/index.php
+++ src/main/index.php
@@ -5,19 +5,3 @@ if (isset($_COOKIE['login']) && isset($_COOKIE['password'])){
     print "Hello {$userData['name']}";
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        
-    </body>
-    </html>
-    <?php
-    else {
-        header("Location: ../");
-    }
-?>
